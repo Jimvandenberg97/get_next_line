22ae9c5361652960ad344b63c5eb173cd8ba73ec
Jim Van den berg <jivan-de@f0r4s17.codam.nl>
2019-11-15 17:03:52 +0100

    What even is this

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jivan-de <jivan-de@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/12 12:29:44 by jivan-de      #+#    #+#                 */
-/*   Updated: 2019/11/15 13:11:40 by jivan-de      ########   odam.nl         */
+/*   Updated: 2019/11/15 17:02:58 by jivan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int		get_next_line(int fd, char **line)
 	static int		result;
 	static int		i;
 	char			*new
+	char			*temp;
 
 	if (!line || fd < 0)
 		return (-1);
@@ -25,16 +26,16 @@ int		get_next_line(int fd, char **line)
 	if (!buf)
 		return (-1);
 	result = read(fd, buf, BUFFER_SIZE);
-	while(ft_strchr(buf, '\n') == NULL);
+	while(ft_strchr(buf, '\n') == NULL && *buf != EOF);
 	{
-		if (ft_strchr(buf, '\n') != NULL)
+		if (ft_strchr(buf, '\n') != NULL || *buf == EOF)
 		{
 			new = ft_strdup(buf);
-			i = ft_strlen(buf OR new);
+			i = ft_strlen(buf OR new '?');
 		}
 		buf++;
 	}
-	temp = ft_strjoin(temp, new);
+	temp = ft_strjoin(buf, new);
 	free(new);
 	if (result > 0)
 		buf[result] = '\0';
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jivan-de <jivan-de@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/12 12:30:53 by jivan-de      #+#    #+#                 */
-/*   Updated: 2019/11/14 17:30:17 by jivan-de      ########   odam.nl         */
+/*   Updated: 2019/11/15 16:19:48 by jivan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,14 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-int		get_next_line(int fd, char **line);
-char	*ft_strchr(const chr *s, int c);
+typedef struct		s_list
+{
+	int				fd;
+	char			*buf;
+	struct s_list	*next;
+}					t_list;
+
+int					get_next_line(int fd, char **line);
+char				*ft_strchr(const chr *s, int c);
 
 #endif
