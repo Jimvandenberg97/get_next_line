bbd5b2ab367ed44ff62945ede89c5c9aa0d19e10
Jim Van den berg <jivan-de@f0r4s21.codam.nl>
2019-11-22 14:15:37 +0100

    Working GNL + bonus, no memory leaks and everything tested

---
+++ get_next_line/get_next_line_utils_bonus.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line_utils_bonus.c                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jivan-de <jivan-de@student.codam.n>          +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/12 12:30:33 by jivan-de      #+#    #+#                 */
+/*   Updated: 2019/11/22 14:01:01 by jivan-de      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char		*d;
+	const char	*s;
+
+	if (n == 0 || dst == src)
+		return (dst);
+	d = (char*)dst;
+	s = (const char*)src;
+	if (!d && !s)
+		return (NULL);
+	while (n)
+	{
+		*d = *s;
+		d++;
+		s++;
+		n--;
+	}
+	return (dst);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	len;
+	void	*mem;
+
+	if (s1 == NULL)
+		return (ft_strdup(""));
+	len = ft_strlen(s1);
+	mem = (char*)malloc(len + 1);
+	if (!mem)
+		return (NULL);
+	ft_memcpy(mem, s1, len + 1);
+	return (mem);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	len_1;
+	size_t	len_2;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len_1 = ft_strlen(s1);
+	len_2 = ft_strlen(s2);
+	str = malloc(len_1 + len_2 + 1);
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s1, len_1 + 1);
+	ft_strlcat(str, s2, len_1 + len_2 + 1);
+	return (str);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	slen;
+
+	if (!dst || !src)
+		return (0);
+	slen = ft_strlen(src);
+	if (slen + 1 < dstsize)
+		ft_memcpy(dst, src, slen + 1);
+	else if (dstsize != 0)
+	{
+		ft_memcpy(dst, src, dstsize - 1);
+		dst[dstsize - 1] = '\0';
+	}
+	return (slen);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	dlen;
+	size_t	slen;
+	size_t	i;
+
+	dlen = ft_strlen(dst);
+	slen = ft_strlen(src);
+	if (dstsize == 0)
+		return (slen);
+	if (dstsize < dlen)
+		slen = slen + dstsize;
+	else
+		slen = slen + dlen;
+	i = 0;
+	while (src[i] != '\0' && dlen < dstsize - 1 && dst != src)
+	{
+		dst[dlen] = src[i];
+		i++;
+		dlen++;
+	}
+	dst[dlen] = '\0';
+	return (slen);
+}
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,24 +6,78 @@
 /*   By: jivan-de <jivan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/15 11:55:55 by jivan-de      #+#    #+#                 */
-/*   Updated: 2019/11/20 10:42:44 by jivan-de      ########   odam.nl         */
+/*   Updated: 2019/11/22 14:09:39 by jivan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 #include <stdio.h>
-#include <unistd.h>
 #include <fcntl.h>
-
+/*
 int		main(int argc, char **argv)
 {
-	int fd;
-	char *line;
-	int status;
+	int		fd;
+	int		fd1;
+	char	*line;
+	int		status;
 
+	if (argc <= 0)
+		return (0);
+	fd1 = open(argv[2], O_RDONLY);
 	fd = open(argv[1], O_RDONLY);
 	status = 1;
-	while ((status = get_next_line(fd, &line)))
-		printf("%s ", line);
+	while ((status = get_next_line(fd, &line)) > 0)
+	{
+		printf("%s\n", line);
+		free(line);
+		while ((status = get_next_line(fd1, &line)) > 0)
+		{
+			printf("%s\n", line);
+			free(line);
+		}
+	}
+	printf("RETURN VALUE: [%d]  |%s\n", status, line);
+	free(line);
+
+	return (0);
+}
+*/
+
+int		main(void)
+{
+	int		fd1, fd2, fd3, fd4, fd5, fd6;
+	char	*line;
+	int		s1, s2, s3, s4, s5, s6;
+
+	fd1 = open("../testfiles/test_nl.txt", O_RDONLY);
+	fd2 = open("../testfiles/test_nonl.txt", O_RDONLY);
+	fd3 = open("../testfiles/test_empty.txt", O_RDONLY);
+	fd4 = open("../testfiles/holy_text.txt", O_RDONLY);
+	fd5 = -1;
+	fd6 = 100;
+	s1 = s2 = s3 = s4 = s5 = s6 = 1;
+	while (s1 > 0 || s2 > 0 || s3 > 0 || s4 > 0)
+	{
+		s1 = get_next_line(fd1, &line);
+		printf("RETURN VALUE [fd1] : [%d]  |%s\n", s1, line);
+		free(line);
+		s2 = get_next_line(fd2, &line);
+		printf("RETURN VALUE [fd2] : [%d]  |%s\n", s2, line);
+		free(line);
+		s3 = get_next_line(fd3, &line);
+		printf("RETURN VALUE [fd3] : [%d]  |%s\n", s3, line);
+		free(line);
+		s4 = get_next_line(fd4, &line);
+		printf("RETURN VALUE [fd4] : [%d]  |%s\n", s4, line);
+		free(line);
+	}
+	printf("RETURN VALUE [fd1] : [%d]  |%s\n", s1, line);
+	printf("RETURN VALUE [fd2] : [%d]  |%s\n", s2, line);
+	printf("RETURN VALUE [fd3] : [%d]  |%s\n", s3, line);
+	printf("RETURN VALUE [fd4] : [%d]  |%s\n", s4, line);
+	s5 = get_next_line(fd5, &line);
+	printf("RETURN VALUE [fd5] : [%d]\n", s5);
+	s6 = get_next_line(fd6, &line);
+	printf("RETURN VALUE [fd6] : [%d]\n", s6);
 	return (0);
 }
